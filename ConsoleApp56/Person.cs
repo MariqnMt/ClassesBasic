79fa2f528dd720c52f3e2dc4b134ed48bee844c4
mashinata123 <bozhidar.petrov.pp26@pgmtplovdiv.info>
2026-02-11 09:04:41 +0200

    Add project files.

---
+++ ConsoleApp56/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    class Person
+    {
+        // Полета (състояние)
+        private string name;
+        private int age;
+
+        // Свойство Name
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Свойство Age
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        // Метод
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Здравей! Казвам се {name} и съм на {age} години.");
+        }
+    }
+}
---
+++ ConsoleApp56/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp56
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Person person = new Person();
+            person.Name = "Майтапчийски";
+            person.Age = 15;
+            person.IntroduceYourself();
+        }
+    }
+}
